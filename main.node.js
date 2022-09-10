39fc3f19f9b1316800a3d84913a9e6eecda0c7ec
sigma-andex <77549848+sigma-andex@users.noreply.github.com>
2022-09-10 18:34:36 +0100

    WIP

---
+++ main.node.js
@@ -0,0 +1,26 @@
+import { init, WASI } from "@wasmer/wasi";
+import { WasmFs } from "@wasmer/wasmfs";
+
+await init();
+
+let wasi = new WASI({
+  env: {
+    // 'ENVVAR1': '1',
+    // 'ENVVAR2': '2'
+  },
+  args: [
+    // 'command', 'arg1', 'arg2'
+  ],
+});
+
+const moduleBytes = fetch("./test.wasm");
+const module = await WebAssembly.compileStreaming(moduleBytes);
+// Instantiate the WASI module
+await wasi.instantiate(module, {});
+
+// Run the start function
+let exitCode = wasi.start();
+//let stdout = wasi.getStdoutString();
+
+// This should print "hello world (exit code: 0)"
+//console.log(`${stdout}(exit code: ${exitCode})`);
---
+++ vite.config.js
@@ -0,0 +1,40 @@
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
+import { defineConfig } from 'vite'
+import wasm from "vite-plugin-wasm";
+import topLevelAwait from "vite-plugin-top-level-await";
+
+export default defineConfig({
+    // ...other config settings
+    optimizeDeps: {
+        esbuildOptions: {
+            // Node.js global to browser globalThis
+            define: {
+                global: 'globalThis'
+            },
+            // Enable esbuild polyfill plugins
+            plugins: [
+                NodeGlobalsPolyfillPlugin({
+                    buffer: true
+                })
+            ]
+        }
+    },
+
+  plugins: [
+    /**
+     * Since 2.x version of this plugin, the `filter` option has been removed.
+     * 
+     * For 1.x (with Vite 2.x):
+     *   By default ALL `.wasm` imports will be transformed to WebAssembly ES module.
+     *   You can also set a filter (function or regex) to match files you want to transform.
+     *   Other files will fallback to Vite's default WASM loader (i.e. You need to call `initWasm()` for them). 
+     *   ```js
+     *   wasm({
+     *     filter: /syntect_bg.wasm$/
+     *   })
+     *   ```
+     */
+    wasm(),
+    topLevelAwait()
+  ]
+})
